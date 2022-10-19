cb95e8704eee8d8a9bea100411c00cf31514813d
Jacob Mendoza <jacob.mendoza@kiubix.com>
2022-10-18 20:17:24 -0500

    pulcerasv1.9.6

--- api/pulceras.php
+++ api/pulceras.php
@@ -49,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
 
     if(isset($id)){
         
-        $sql = $conn->query("DELETE FROM pulseras WHERE id = $id");
+        $sql = $conn->query("DELETE FROM pulseras WHERE id = '$id'");
 
         if($sql) {
             exit(json_encode(array('status' => 'success')));
